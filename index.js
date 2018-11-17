b598e57fd92c5c5881bd5810a73b9512e96bccc9
Roeland Jago Douma <roeland@famdouma.nl>
2018-11-17 20:30:55 +0100

    Split up code to small modular pieces
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const pr = require('./lib/pr.js')
+const comment = require('./lib/comment.js')
+const backport = require('./lib/backport.js')
 const getCommits = require('./lib/commits.js')
-const git = require('./lib/git.js')
-const githubApp = require('github-app')
-
+const getToken = require('./lib/token.js')
 
 module.exports = app => {
   app.on('issue_comment.created', async context => {
@@ -13,55 +14,56 @@ module.exports = app => {
       return
     }
 
-    // Check for the command
-    const command = /^\/backport to ([a-zA-Z0-9]+)$/im
-    const match = command.exec(payload.comment.body)
-    if (match === null) {
-      app.log("Not for us")
+    const target = comment.match(payload.comment.body);
+    if (target === false) {
+      app.log('Ignore')
       return;
     }
 
-    // +1 the request
-    {
-      const params = context.repo({content: '+1', comment_id: payload.comment.id});
-      await context.github.reactions.createForIssueComment(params);
-    }
+    await comment.plusOne(payload.comment.id)
 
-    // Check if the PR is merged already
-    const params = context.repo({number: payload.issue.number});
-    const pr = await context.github.pullRequests.get(params)
-
-    if (pr.data.merged === true) {
-      app.log("PR IS MERGED LETS DO THE BACKPORT!")
+    // TODO: set label
+    
+    if (!(await pr.isMerged(context, payload.issue.number))) {
+      app.log("PR is not yet merged just carry on")
       return
     }
 
-    app.log("PR NOT YET MERGED lets wait till it is merged")
   })
 
   app.on('pull_request.closed', async context => {
     const payload = context.payload
     const installationId = context.payload.installation.id
 
-    const auth = githubApp({
-      id: process.env.APP_ID,
-      cert: process.env.PRIVATE_KEY
-    })
-    const token = await auth.createToken(installationId)
-
-    app.log(token)
-
     const params = context.issue()
     const comments  = await context.github.issues.getComments(params)
 
-    for (const {body} of comments.data) {
-      app.log(body)
+    // Obtain all targets
+    let targets = []
+    for (const {body, id} of comments.data) {
+      const target = comment.match(body)
+      if (target !== false) {
+        targets.push(target)
+        await comment.plusOne(context, id)
+
+        // TODO: Set label
+      }
     }
 
-    const commits = await getCommits(context)
+    app.log(targets)
+    const origPRnr = context.issue().number
+    app.log(origPRnr)
+
+    const token = await getToken(installationId)
+    const commits = await getCommits(context);
+
+    app.log(token)
     app.log(commits)
 
-    const branch = await git(app.log, context, token.data.token, commits, 'stable14')
+    for (const target of targets) {
+      await backport(app.log, context, origPRnr, target, token, commits)
+    }
     
+    // TODO: Clear label
   })
 }
